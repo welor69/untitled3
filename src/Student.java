3f0e908929590cb54cfce855bb2a9519218e5aa2
welor69 <azsalm.shop@gmail.com>
2026-03-05 23:00:26 +0600

    Add files via upload

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    String name, group;
+    int age;
+    double gpa;
+    Student(String name, String group, int age,double gpa){
+        this.name = name;
+        this.group = group;
+        this.age = age;
+        this.gpa = gpa;
+    }
+    public String getName(){
+        return name;
+    }
+    public double getGpa(){
+        return getGpa();
+    }
+}
